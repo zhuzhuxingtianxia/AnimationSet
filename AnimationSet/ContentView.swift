066b88ef47c805e0112e7429b965cb9a3db4cac2
zhuzijia <873391579@qq.com>
2021-02-23 15:04:30 +0800

    Update ContentView.swift

--- AnimationSet/ContentView.swift
+++ AnimationSet/ContentView.swift
@@ -7,27 +7,56 @@
 
 import SwiftUI
 
+enum ContentAnimation {
+    case HideAnimation
+    case ShowAnimation
+    case LoadingAnimation
+    case SpinAnimation
+}
+
+struct Item: Hashable {
+    var title: String
+    var key: ContentAnimation
+}
+
+
 struct ContentView: View {
     
-    let array = [["title":"隐式动画","key":"HideAnimation"],
-                 ["title":"显式动画","key":"ShowAnimation"],
-                 ["title":"加载动画","key":"LoadingAnimation"],
-                 ["title":"⦁⦁⦁动画","key":"SpinAnimation"],
+    let array = [Item(title: "隐式动画", key: .HideAnimation),
+                 Item(title: "显式动画", key: .ShowAnimation),
+                 Item(title: "加载动画", key: .LoadingAnimation),
+                 Item(title: "⦁⦁⦁动画", key: .SpinAnimation)
     ]
     
     var body: some View {
         NavigationView {
             List(array, id: \.self) { item in
                 NavigationLink(
-                    destination: SpinAnimation(),
+                    destination: presentNavigation(item.key),
                     label: {
-                        Text(item["title"]!).padding()
+                        Text(item.title).padding()
                     })
                 
       
             }
             .navigationBarTitle("动画锦集")
             
+        }
+    }
+    
+    @ViewBuilder
+    func presentNavigation(_ key: ContentAnimation) -> some View {
+        switch key {
+        case .HideAnimation:
+            HideAnimation()
+        case .ShowAnimation:
+            ShowAnimation()
+        case .LoadingAnimation:
+            LoadingAnimation()
+        case .SpinAnimation:
+            SpinAnimation()
+        default:
+            EmptyView()
         }
     }
 }
